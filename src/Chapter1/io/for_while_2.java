197bbbfc88dbbb839af5ce6cf724a1927909ff58
JaeSeok <ljaeseok02@gmail.com>
2026-02-23 14:36:47 +0900

    commit message

---
+++ src/Chapter1/io/for_while_2.java
@@ -0,0 +1,11 @@
+package Chapter1.io;
+
+public class for_while_2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i+= 2) {
+            System.out.println(i);
+        }
+    }
+}
