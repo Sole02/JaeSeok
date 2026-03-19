3dc7b3091148f790980d1b25c3bfa9d8822287a0
Sole02 <ljaeseok02@gmail.com>
2026-03-19 12:56:29 +0900

    STEP 1. 양의 정수 입력 받기

---
+++ calculator/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        int num2 = 0;
+
+        System.out.println("숫자를 입력하세요.");
+        num = sc.nextInt();
+
+        System.out.println("숫자를 입력하세요.");
+        num2 = sc.nextInt();
+    }
+}
