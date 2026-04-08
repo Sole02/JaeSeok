334505e7bbc38e25f79914c4a1c80bdbe9e97e98
Sole02 <ljaeseok02@gmail.com>
2026-04-08 17:23:47 +0900

    feat: 리스트 기반 랜덤 영화 뽑기 및 이미지 매핑 기능 구현

---
+++ movie-picker/src/test/java/com/example/moviess/MoviessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
