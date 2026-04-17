8f594496c41b9c8eb7f226b0872860c6ddaf796d
Sole02 <ljaeseok02@gmail.com>
2026-04-17 21:17:51 +0900

    feat: 일정 생성 API 서비스 연결 및 응답 반환 추가

--- schedule-app/schedule-app/src/main/java/com/example/schedule_app/controller/ScheduleController.java
+++ schedule-app/schedule-app/src/main/java/com/example/schedule_app/controller/ScheduleController.java
@@ -1,24 +1,29 @@
 package com.example.schedule_app.controller;
 
-import com.example.schedule_app.dto.ScheduleControllerRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.schedule_app.dto.request.ScheduleCreateRequestDto;
+import com.example.schedule_app.dto.response.ScheduleCreateResponseDto;
+import com.example.schedule_app.service.ScheduleService;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/schedule")
+@RestController // 1. 컴포넌트 스캔
+@RequestMapping("/schedule") // 2. 리퀘스트 맵핑
 public class ScheduleController {
 
+    // 속성
 
 
+    // 생성자
+    private ScheduleService scheduleService;
 
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
 
+    // 기능
 
-
-
+    // 일정 등록
     @PostMapping
-    public void creatSchedule(@RequestBody ScheduleControllerRequestDto requestDto) {
-        System.out.println("ScheduleController.creatSchedule()");
+    public ScheduleCreateResponseDto createSchedule(@RequestBody ScheduleCreateRequestDto requestDto) {
+        return scheduleService.createSchedule(requestDto);
     }
 }
