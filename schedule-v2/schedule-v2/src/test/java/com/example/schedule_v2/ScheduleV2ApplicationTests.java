21172d41a2fb1264705353c4fa31abf80e57af4b
Sole02 <ljaeseok02@gmail.com>
2026-04-19 17:04:20 +0900

    init: 프로젝트 초기 구조 설정

---
+++ schedule-v2/schedule-v2/src/test/java/com/example/schedule_v2/ScheduleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedule_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
