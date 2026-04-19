e100e1c2c5c9881af47370c24b9c943222a4c6aa
Sole02 <ljaeseok02@gmail.com>
2026-04-19 17:37:36 +0900

    feat: 일정 생성, 엔티티 추가

---
+++ schedule-v2/schedule-v2/src/main/java/com/example/schedule_v2/entity/Schedule.java
@@ -0,0 +1,34 @@
+package com.example.schedule_v2.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "schedule")
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    // 속성
+    private Long id;
+    private String title;
+    private String content;
+
+    // 생성자
+    public Schedule() {}
+
+    public Schedule(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // 기능
+    public Long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+}
