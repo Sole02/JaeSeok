f2adf1a43c0b39291b1ee473c713ab468b403ab4
Sole02 <ljaeseok02@gmail.com>
2026-04-20 14:51:34 +0900

    feat: 일정 생성 서비스 추가 및 컨트롤러 연결

---
+++ schedule-v2/schedule-v2/src/main/java/com/example/schedule_v2/service/ScheduleService.java
@@ -0,0 +1,32 @@
+package com.example.schedule_v2.service;
+
+import com.example.schedule_v2.dto.request.ScheduleCreateRequestDto;
+import com.example.schedule_v2.dto.response.ScheduleCreateResponseDto;
+import com.example.schedule_v2.entity.Schedule;
+import com.example.schedule_v2.repository.ScheduleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleService {
+
+    // 속성
+    private final ScheduleRepository scheduleRepository;
+
+    // 생성자
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    // 기능
+    public ScheduleCreateResponseDto createSchedule (ScheduleCreateRequestDto requestDto) {
+        String foundTitle = requestDto.getTitle();
+        String foundContent = requestDto.getContent();
+        Schedule newSchedule = new Schedule(foundTitle, foundContent);
+        Schedule savedSchedule = scheduleRepository.save(newSchedule);
+        Long savedScheduleId = savedSchedule.getId();
+        String savedScheduleTitle = savedSchedule.getTitle();
+        String savedScheduleContent = savedSchedule.getContent();
+        ScheduleCreateResponseDto result = new ScheduleCreateResponseDto(savedScheduleId, savedScheduleTitle, savedScheduleContent);
+        return result;
+    }
+}
